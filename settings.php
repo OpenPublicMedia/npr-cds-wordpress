742f550c1621d53b1ca919438a948f47c682b2c3
Jared Counts <asentenceismyname@gmail.com>
2024-04-18 17:16:32 -0500

    Redesigned the settings screens with more use of the WP Settings API to break up some of the sections

--- settings.php
+++ settings.php
@@ -40,12 +40,57 @@ function npr_cds_options_general(): void { ?>
 			}
 		}
 	</style>
-	<h1>NPR Content Distribution Service Settings</h1>
+	<h1>NPR CDS: General Settings</h1>
 	<form action="options.php" method="post">
-	<?php
-		settings_fields( 'npr_cds' );
-		do_settings_sections( 'npr_cds' );
-		submit_button(); ?>
+	<?php settings_fields( 'npr_cds' ); ?>
+	<div id="poststuff">
+		<div id="post-body" class="metabox-holder columns-2">
+			<div id="post-body-content">
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('CDS Settings', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds', 'npr_cds_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('Organization Settings', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds', 'npr_cds_org_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('Theme Settings', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds', 'npr_cds_theme_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('Image Settings', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds', 'npr_cds_image_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br class="clear" />
+	<?php submit_button(); ?>
 	</form><?php
 }
 function npr_cds_options_multi(): void { ?>
@@ -67,9 +112,9 @@ function npr_cds_options_multi(): void { ?>
 			grid-template-columns: 10rem auto;
 			gap: 1rem;
 			align-items: center;
-			padding-bottom: 1rem;
-			border-bottom: 1px solid #808080;
-			margin-bottom: 1rem;
+		}
+		.cds-query-details .form-table tr + tr {
+			border-top: 1px solid #808080;
 		}
 		@media screen and (max-width: 500px) {
 			.npr-cds-query h4 {
@@ -83,13 +128,49 @@ function npr_cds_options_multi(): void { ?>
 			}
 		}
 	</style>
-	<h1>NPR Content Distribution Service Settings</h1>
+	<h1>NPR CDS: Get Multi Settings</h1>
 	<form action="options.php" method="post">
-		<?php
-			settings_fields( 'npr_cds_get_multi_settings' );
-			do_settings_sections( 'npr_cds_get_multi_settings' );
-			submit_button();
-		?>
+	<?php settings_fields( 'npr_cds_get_multi_settings' ); ?>
+	<!--			do_settings_sections( 'npr_cds_get_multi_settings' );-->
+
+	<div id="poststuff">
+		<div id="post-body" class="metabox-holder columns-2">
+			<div id="post-body-content">
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('Query Settings', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds_get_multi_settings', 'npr_cds_get_multi_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div class="postbox cds-query-details">
+						<div class="postbox-header"><h2><?php _e('Query Details', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds_get_multi_settings', 'npr_cds_query_details' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('Cron Settings', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds_get_multi_settings', 'npr_cds_cron_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br class="clear" />
+	<?php submit_button(); ?>
 	</form><?php
 }
 function npr_cds_options_push_mapping(): void { ?>
@@ -108,18 +189,32 @@ function npr_cds_options_push_mapping(): void { ?>
 			}
 		}
 	</style>
-	<h1>NPR Content Distribution Service Settings</h1>
+	<h1>NPR CDS: Push Mapping</h1>
 	<form action="options.php" method="post">
-		<?php
-			settings_fields( 'npr_cds_push_mapping' );
-			do_settings_sections( 'npr_cds_push_mapping' );
-			submit_button();
-		?>
+	<?php settings_fields( 'npr_cds_push_mapping' ); ?>
+	<div id="poststuff">
+		<div id="post-body" class="metabox-holder columns-2">
+			<div id="post-body-content">
+				<div>
+					<div class="postbox">
+						<div class="postbox-header"><h2><?php _e('CDS Push Mapping', 'npr-content-distribution-service' ); ?></h2></div>
+						<div class="inside">
+							<table class="form-table">
+								<?php do_settings_fields( 'npr_cds_push_mapping', 'npr_cds_push_settings' ); ?>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div>
+				</div>
+			</div>
+			<br class="clear" />
+			<?php submit_button(); ?>
 	</form><?php
 }
 
 function npr_cds_settings_init(): void {
-	// NPR CDS Settings Group
+	// CDS: General Settings
 	add_settings_section( 'npr_cds_settings', 'General Settings', 'npr_cds_settings_callback', 'npr_cds' );
 
 	add_settings_field( 'npr_cds_token', 'CDS Token', 'npr_cds_token_callback', 'npr_cds', 'npr_cds_settings' );
@@ -131,52 +226,66 @@ function npr_cds_settings_init(): void {
 	add_settings_field( 'npr_cds_push_url', 'Push URL', 'npr_cds_push_url_callback', 'npr_cds', 'npr_cds_settings' );
 	register_setting( 'npr_cds', 'npr_cds_push_url', [ 'sanitize_callback' => 'npr_cds_validation_callback_pull_url' ] );
 
-	add_settings_field( 'npr_cds_org_id', 'Org ID', 'npr_cds_org_id_callback', 'npr_cds', 'npr_cds_settings' );
+	// CDS: Org Settings
+	add_settings_section( 'npr_cds_org_settings', 'Organization Settings', 'npr_cds_org_settings_callback', 'npr_cds' );
+
+	add_settings_field( 'npr_cds_org_id', 'Org ID', 'npr_cds_org_id_callback', 'npr_cds', 'npr_cds_org_settings' );
 	register_setting( 'npr_cds', 'npr_cds_org_id', [ 'sanitize_callback' => 'npr_cds_validation_callback_org_id' ] );
 
-	add_settings_field( 'npr_cds_prefix', 'Document Prefix', 'npr_cds_prefix_callback', 'npr_cds', 'npr_cds_settings' );
+	add_settings_field( 'npr_cds_prefix', 'Document Prefix', 'npr_cds_prefix_callback', 'npr_cds', 'npr_cds_org_settings' );
 	register_setting( 'npr_cds', 'npr_cds_prefix', [ 'sanitize_callback' => 'npr_cds_validation_callback_prefix' ] );
 
-	add_settings_field( 'npr_cds_query_use_featured', 'Theme uses Featured Image', 'npr_cds_query_use_featured_callback', 'npr_cds', 'npr_cds_settings' );
+	// CDS: Theme Settings
+	add_settings_section( 'npr_cds_theme_settings', 'Theme Settings', 'npr_cds_settings_callback', 'npr_cds' );
+
+	add_settings_field( 'npr_cds_query_use_featured', 'Theme uses Featured Image', 'npr_cds_query_use_featured_callback', 'npr_cds', 'npr_cds_theme_settings' );
 	register_setting( 'npr_cds', 'npr_cds_query_use_featured', [ 'sanitize_callback' => 'npr_cds_validation_callback_checkbox' ] );
 
-	add_settings_field( 'npr_cds_pull_post_type', 'NPR Default Pull Post Type', 'npr_cds_pull_post_type_callback', 'npr_cds', 'npr_cds_settings' );
+	add_settings_field( 'npr_cds_pull_post_type', 'NPR Default Pull Post Type', 'npr_cds_pull_post_type_callback', 'npr_cds', 'npr_cds_theme_settings' );
 	register_setting( 'npr_cds', 'npr_cds_pull_post_type' );
 
-	add_settings_field( 'npr_cds_push_post_type', 'NPR Push Post Type', 'npr_cds_push_post_type_callback', 'npr_cds', 'npr_cds_settings' );
+	add_settings_field( 'npr_cds_push_post_type', 'NPR Push Post Type', 'npr_cds_push_post_type_callback', 'npr_cds', 'npr_cds_theme_settings' );
 	register_setting( 'npr_cds', 'npr_cds_push_post_type' );
 
-	add_settings_field( 'npr_cds_push_default', 'NPR Push to CDS Default', 'npr_cds_push_default_callback', 'npr_cds', 'npr_cds_settings' );
+	add_settings_field( 'npr_cds_push_default', 'NPR Push to CDS Default', 'npr_cds_push_default_callback', 'npr_cds', 'npr_cds_theme_settings' );
 	register_setting( 'npr_cds', 'npr_cds_push_default' );
 
-	add_settings_field( 'npr_cds_image_width', 'Max Image Width', 'npr_cds_image_width_callback', 'npr_cds', 'npr_cds_settings' );
+	// CDS: Image Settings
+	add_settings_section( 'npr_cds_image_settings', 'Image Settings', 'npr_cds_settings_callback', 'npr_cds' );
+
+	add_settings_field( 'npr_cds_image_width', 'Max Image Width', 'npr_cds_image_width_callback', 'npr_cds', 'npr_cds_image_settings' );
 	register_setting( 'npr_cds', 'npr_cds_image_width' );
 
-	add_settings_field( 'npr_cds_image_quality', 'Image Quality', 'npr_cds_image_quality_callback', 'npr_cds', 'npr_cds_settings' );
+	add_settings_field( 'npr_cds_image_quality', 'Image Quality', 'npr_cds_image_quality_callback', 'npr_cds', 'npr_cds_image_settings' );
 	register_setting( 'npr_cds', 'npr_cds_image_quality' );
 
-	add_settings_field( 'npr_cds_image_format', 'Image Format', 'npr_cds_image_format_callback', 'npr_cds', 'npr_cds_settings' );
+	add_settings_field( 'npr_cds_image_format', 'Image Format', 'npr_cds_image_format_callback', 'npr_cds', 'npr_cds_image_settings' );
 	register_setting( 'npr_cds', 'npr_cds_image_format' );
 
-	// NPR CDS Get Multi Settings Group
+	// CDS: Get Multi Settings
 	add_settings_section( 'npr_cds_get_multi_settings', 'Multiple Get Settings', 'npr_cds_get_multi_settings_callback', 'npr_cds_get_multi_settings' );
 
 	add_settings_field( 'npr_cds_num', 'Number of things to get', 'npr_cds_num_multi_callback', 'npr_cds_get_multi_settings', 'npr_cds_get_multi_settings' );
 	register_setting( 'npr_cds_get_multi_settings', 'npr_cds_num', [ 'type' => 'integer', 'sanitize_callback' => 'npr_cds_num_validation' ] );
 
+	// CDS: Query Details
+	add_settings_section( 'npr_cds_query_details', 'Query Details', 'npr_cds_query_details_callback', 'npr_cds_get_multi_settings' );
 	$num = get_option( 'npr_cds_num', 1 );
 	for ( $i = 0; $i < $num; $i++ ) {
-		add_settings_field( 'npr_cds_query_' . $i, 'Query ' . $i, 'npr_cds_query_callback', 'npr_cds_get_multi_settings', 'npr_cds_get_multi_settings', $i );
+		add_settings_field( 'npr_cds_query_' . $i, 'Query ' . $i, 'npr_cds_query_callback', 'npr_cds_get_multi_settings', 'npr_cds_query_details', $i );
 		register_setting( 'npr_cds_get_multi_settings', 'npr_cds_query_' . $i, [ 'type' => 'array', 'default' => [ 'filters' => '', 'sorting' => '', 'publish' => '', 'category' => '', 'tags' => '' ] ] );
 	}
 
-	add_settings_field( 'npr_cds_query_run_multi', 'Run the queries on saving changes', 'npr_cds_query_run_multi_callback', 'npr_cds_get_multi_settings', 'npr_cds_get_multi_settings' );
+	// CDS: Cron Settings
+	add_settings_section( 'npr_cds_cron_settings', 'Cron Settings', 'npr_cds_settings_callback', 'npr_cds_get_multi_settings' );
+
+	add_settings_field( 'npr_cds_query_run_multi', 'Run the queries on saving changes', 'npr_cds_query_run_multi_callback', 'npr_cds_get_multi_settings', 'npr_cds_cron_settings' );
 	register_setting( 'npr_cds_get_multi_settings', 'npr_cds_query_run_multi', [ 'sanitize_callback' => 'npr_cds_validation_callback_checkbox' ] );
 
-	add_settings_field( 'npr_cds_query_multi_cron_interval', 'Interval to run Get Multi cron', 'npr_cds_query_multi_cron_interval_callback', 'npr_cds_get_multi_settings', 'npr_cds_get_multi_settings' );
+	add_settings_field( 'npr_cds_query_multi_cron_interval', 'Interval to run Get Multi cron', 'npr_cds_query_multi_cron_interval_callback', 'npr_cds_get_multi_settings', 'npr_cds_cron_settings' );
 	register_setting( 'npr_cds_get_multi_settings', 'npr_cds_query_multi_cron_interval', [ 'type' => 'integer' ] );
 
-	// NPR CDS Push Settings Group
+	// CDS: Push Settings
 	add_settings_section( 'npr_cds_push_settings', 'Metadata Settings', 'npr_cds_push_settings_callback', 'npr_cds_push_mapping' );
 
 	add_settings_field( 'npr_cds_push_use_custom_map', 'Use Custom Settings', 'npr_cds_use_custom_mapping_callback', 'npr_cds_push_mapping', 'npr_cds_push_settings' );
@@ -208,7 +317,7 @@ function npr_cds_push_settings_callback(): void { ?>
 	<p>Use this page to map your custom WordPress Meta fields to fields sent to the NPR CDS, and vice versa. Clicking the <strong>Use Custom Settings</strong> checkbox will enable these mappings. If you wish to use the default mapping for a field, select &mdash; default &mdash; and we will use the obvious WordPress field.</p>
 	<p>Select for the Meta fields for the <strong><?php echo esc_html( npr_cds_get_push_post_type() ); ?></strong> post
 		type.</p> <?php
- }
+}
 
 function npr_cds_get_multi_settings_callback(): void {
 	$run_multi = get_option( 'npr_cds_query_run_multi' );
@@ -303,7 +412,7 @@ function npr_cds_prefix_callback(): void {
 function npr_cds_query_use_featured_callback(): void {
 	$use_featured = get_option( 'npr_cds_query_use_featured' );
 	$check_box_string = '<input id="npr_cds_query_use_featured" name="npr_cds_query_use_featured" type="checkbox" value="true"' .
-		( $use_featured ? ' checked="checked"' : '' ) . ' />';
+	                    ( $use_featured ? ' checked="checked"' : '' ) . ' />';
 
 	echo npr_cds_esc_html( '<p>' . $check_box_string . " If your theme uses the featured image, checking this box will remove the lead image from imported posts.</p>" );
 }
@@ -322,8 +431,8 @@ function npr_cds_push_post_type_callback(): void {
 function npr_cds_push_default_callback(): void {
 	$push_default = get_option( 'npr_cds_push_default', '1' );
 	$check_box_string = '<select id="npr_cds_push_default" name="npr_cds_push_default"><option value="1"' . ( $push_default == '1' ? ' selected' : '' ) . '>Checked</option>' .
-	'<option value="0"' . ( $push_default == '0' ? ' selected' : '' ) . '>Not Checked</option>' .
-	'</select>';
+	                    '<option value="0"' . ( $push_default == '0' ? ' selected' : '' ) . '>Not Checked</option>' .
+	                    '</select>';
 	echo npr_cds_esc_html( '<p>' . $check_box_string . '</p><p><em>When creating a new post in your NPR Push Post Type, do you want the "Push to NPR CDS" box to be checked by default or not?</em></p>' );
 }
 
@@ -359,16 +468,16 @@ function npr_cds_query_callback( $i ): void {
 		$post_types = get_post_types();
 
 		$output = '<div class="npr-cds-query"><h4>Filters</h4><div><p><input type="text" value="' . $query['filters'] . '" name="npr_cds_query_' . $i . '[filters]" placeholder="profileIds=renderable&collectionIds=1002" /></p>' .
-			'<p><em>A list of available filtering options can be found <a href="https://npr.github.io/content-distribution-service/querying/filtering.html">in the CDS documentation</a></em></p></div>' .
-			'<h4>Sorting</h4><div><p><input type="text" value="' . $query['sorting'] . '" name="npr_cds_query_' . $i . '[sorting]" placeholder="sort=<type>[:<direction>]" /></p>' .
-			'<p><em>A list of available sorting query parameters can be found <a href="https://npr.github.io/content-distribution-service/querying/sorting.html">in the CDS documentation</a></em></p></div>' .
-			'<h4>Publish or Save as Draft?</h4> ' .
-				'<div><select id="npr_cds_query_' . $i . '[publish]" name="npr_cds_query_' . $i . '[publish]">' .
-					'<option value="Publish"' . ( $query['publish'] == 'Publish' ? ' selected' : '' ) . '>Publish</option>' .
-					'<option value="Draft"' . ( $query['publish'] == 'Draft' ? ' selected' : '' ) . '>Draft</option>' .
-				'</select></div>' .
-			'<h4>Save as post type? (Leave blank for default)</h4>' .
-			npr_cds_show_post_types_select( 'npr_cds_query_' . $i . '[pull_type]', $post_types, true );
+		          '<p><em>A list of available filtering options can be found <a href="https://npr.github.io/content-distribution-service/querying/filtering.html">in the CDS documentation</a></em></p></div>' .
+		          '<h4>Sorting</h4><div><p><input type="text" value="' . $query['sorting'] . '" name="npr_cds_query_' . $i . '[sorting]" placeholder="sort=<type>[:<direction>]" /></p>' .
+		          '<p><em>A list of available sorting query parameters can be found <a href="https://npr.github.io/content-distribution-service/querying/sorting.html">in the CDS documentation</a></em></p></div>' .
+		          '<h4>Publish or Save as Draft?</h4> ' .
+		          '<div><select id="npr_cds_query_' . $i . '[publish]" name="npr_cds_query_' . $i . '[publish]">' .
+		          '<option value="Publish"' . ( $query['publish'] == 'Publish' ? ' selected' : '' ) . '>Publish</option>' .
+		          '<option value="Draft"' . ( $query['publish'] == 'Draft' ? ' selected' : '' ) . '>Draft</option>' .
+		          '</select></div>' .
+		          '<h4>Save as post type? (Leave blank for default)</h4>' .
+		          npr_cds_show_post_types_select( 'npr_cds_query_' . $i . '[pull_type]', $post_types, true );
 		if ( $optionType == 'post' ) {
 			$args = [
 				'show_option_none'	=> __( 'Select category', 'npr-content-distribution-service' ),
@@ -385,7 +494,7 @@ function npr_cds_query_callback( $i ): void {
 			$output .= '<h4>Add Category</h4><div>' . $select . '</div>';
 		}
 		$output .= '<h4>Add Tags</h4><div><p><input type="text" value="' . $query['tags'] . '" name="npr_cds_query_' . $i . '[tags]" placeholder="pepperoni,pineapple,mozzarella" /></p>' .
-			'<p><em>Add tag(s) to each story pulled from NPR (comma separated).</em></p>';
+		           '<p><em>Add tag(s) to each story pulled from NPR (comma separated).</em></p>';
 		echo npr_cds_esc_html( $output );
 	}
 }
@@ -419,7 +528,7 @@ function npr_cds_query_multi_cron_interval_callback(): void {
 function npr_cds_use_custom_mapping_callback(): void {
 	$use_custom = get_option( 'npr_cds_push_use_custom_map' );
 	$check_box_string = '<input id="npr_cds_push_use_custom_map" name="npr_cds_push_use_custom_map" type="checkbox" value="true"' .
-		( $use_custom ? ' checked="checked"' : '' ) . ' />';
+	                    ( $use_custom ? ' checked="checked"' : '' ) . ' />';
 	echo npr_cds_esc_html( $check_box_string );
 }
 
